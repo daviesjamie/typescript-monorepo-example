2c35751af1838c39dd62c25b7c2bb4e83da04f0f
Jamie Davies <jamie@jagd.me>
2023-08-23 08:56:46 +0100

    Make the api package depend on and import the logic package

--- api/src/api.ts
+++ api/src/api.ts
@@ -1,12 +1,28 @@
+import addBook from "@monorepo/logic/addBook";
+import books from "@monorepo/logic/books";
 import express from "express";
-
+import BookResponse from "./interfaces/BookResponse";
 import MessageResponse from "./interfaces/MessageResponse";
 
 const router = express.Router();
+const currentBooks = books;
 
 router.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
     message: "API - 👋",
+  });
+});
+
+router.get<{}, BookResponse>("/books", (req, res) => {
+  res.json({
+    books: currentBooks,
+  });
+});
+
+router.get<{}, MessageResponse>("/books/add", (req, res) => {
+  addBook(currentBooks);
+  res.json({
+    message: "Added book!",
   });
 });
 
---
+++ api/src/interfaces/BookResponse.ts
@@ -0,0 +1,5 @@
+import Book from "@monorepo/logic/types/Book";
+
+export default interface BookResponse {
+  books: Book[];
+}
